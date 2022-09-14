4a355c7e8e421ed69cc39c74eeeadac4fc817199
Jason Otter <jasonotter17@gmail.com>
2022-09-14 15:50:48 -0700

    uninstall bootstrap-4-react, install react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import { Alert } from "bootstrap-4-react";
 
 function App() {
   return (
     <div>
       <h1>Expense Tracker</h1>
-      <Alert primary>Test</Alert>
     </div>
   );
 }
