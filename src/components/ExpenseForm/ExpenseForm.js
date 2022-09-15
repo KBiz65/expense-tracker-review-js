e017d87fcd267a3db7152f3cc6e920b62205c6c6
Jason Otter <jasonotter17@gmail.com>
2022-09-15 11:26:33 -0700

    add basic form input gathering

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -4,18 +4,48 @@ import "./ExpenseForm.css";
 
 function ExpenseForm() {
   const [validated, setValidated] = useState(false);
+  const [vendor, setVendor] = useState("");
+  const [date, setDate] = useState("");
+  const [amount, setAmount] = useState("");
+  const [desc, setDesc] = useState("");
 
   const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
     const form = e.currentTarget;
+
     if (!form.checkValidity()) {
-      e.preventDefault();
-      e.stopPropagation();
-      console.log("form is not valid");
+      console.log("invalid form");
     } else {
-      console.log("form is valid!");
-      e.preventDefault();
+      console.log("form is valid");
+
+      // log data to see it...
+      console.log(vendor);
+      console.log(date);
+      console.log(amount);
+      console.log(desc);
+
+      // create a new expense...
+
+      // reset the form...
     }
     setValidated(true);
+  };
+
+  const vendorChangeHandler = (e) => {
+    setVendor(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    setDate(e.target.value);
+  };
+
+  const amountChangeHandler = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const descChangeHandler = (e) => {
+    setDesc(e.target.value);
   };
 
   return (
@@ -30,11 +60,21 @@ function ExpenseForm() {
         <Row>
           <Form.Group as={Col}>
             <Form.Label>Vendor</Form.Label>
-            <Form.Control required type="text" />
+            <Form.Control
+              required
+              type="text"
+              onChange={vendorChangeHandler}
+              className="input-vendor"
+            />
           </Form.Group>
           <Form.Group as={Col}>
             <Form.Label>Date</Form.Label>
-            <Form.Control required type="date" />
+            <Form.Control
+              required
+              type="date"
+              onChange={dateChangeHandler}
+              className="input-date"
+            />
           </Form.Group>
         </Row>
         <Row>
@@ -46,11 +86,17 @@ function ExpenseForm() {
               min={0.01}
               step={0.01}
               placeholder={"0.00"}
+              onChange={amountChangeHandler}
+              className="input-amount"
             />
           </Form.Group>
           <Form.Group as={Col}>
             <Form.Label>Description (Optional)</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control
+              type="text"
+              onChange={descChangeHandler}
+              className="input-desc"
+            />
           </Form.Group>
         </Row>
         <Button className="submit-btn" variant="primary" type="submit">
