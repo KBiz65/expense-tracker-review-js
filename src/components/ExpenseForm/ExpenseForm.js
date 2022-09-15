4bc6646b279e07e784d18f48caef84924317aba6
Jason Otter <jasonotter17@gmail.com>
2022-09-15 10:58:13 -0700

    add basic form validation

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,25 +1,52 @@
+import { useState } from "react";
 import { Button, Card, Form, Col, Row } from "react-bootstrap";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (!form.checkValidity()) {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log("form is not valid");
+    } else {
+      console.log("form is valid!");
+      e.preventDefault();
+    }
+    setValidated(true);
+  };
+
   return (
     <Card>
-      <h2>Expense Form Here</h2>
-      <Form className="expense-form">
+      <h2>Add New Expense</h2>
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+        className="expense-form"
+      >
         <Row>
           <Form.Group as={Col}>
             <Form.Label>Vendor</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control required type="text" />
           </Form.Group>
           <Form.Group as={Col}>
             <Form.Label>Date</Form.Label>
-            <Form.Control type="date" />
+            <Form.Control required type="date" />
           </Form.Group>
         </Row>
         <Row>
           <Form.Group as={Col}>
             <Form.Label>Amount</Form.Label>
-            <Form.Control type="number"></Form.Control>
+            <Form.Control
+              required
+              type="number"
+              min={0.01}
+              step={0.01}
+              placeholder={"0.00"}
+            />
           </Form.Group>
           <Form.Group as={Col}>
             <Form.Label>Description (Optional)</Form.Label>
