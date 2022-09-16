454c35ee021720952432571aa4479ee13adaf8af
Jason Otter <jasonotter17@gmail.com>
2022-09-16 15:07:34 -0700

    add edit expense functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Row, Col, Modal } from "react-bootstrap";
 import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
 import ExpenseTable from "./components/ExpenseTable/ExpenseTable";
+import EditExpenseForm from "./components/EditExpenseForm/EditExpenseForm";
 import "./App.css";
-import { useState } from "react";
 
 const fake_data = [
   {
@@ -23,6 +24,11 @@ const fake_data = [
 
 function App() {
   const [expenses, setExpenses] = useState(fake_data);
+  const [expenseToEdit, setExpenseToEdit] = useState("");
+
+  const [show, setShow] = useState(false);
+  const openEditFormModal = () => setShow(true);
+  const closeEditFormModal = () => setShow(false);
 
   const newExpenseDataHandler = (newExpenseData) => {
     setExpenses((prevExpenses) => {
@@ -36,8 +42,15 @@ function App() {
         return prevExpenses.filter((expense) => expense.id !== id);
       });
     } else if (option === "edit") {
-      console.log("still working on edit");
+      setExpenseToEdit(expenses.find((elem) => elem.id === id));
+      openEditFormModal();
     }
+  };
+
+  const editExpenseDataHandler = (editedExpenseData) => {
+    const i = expenses.findIndex((elem) => elem.id === editedExpenseData.id);
+    expenses[i] = editedExpenseData;
+    closeEditFormModal();
   };
 
   return (
@@ -52,6 +65,18 @@ function App() {
           />
         </Col>
       </Row>
+      <Modal size="lg" show={show} onHide={closeEditFormModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Expense</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <EditExpenseForm
+            data={expenseToEdit}
+            onEditExpenseSubmit={editExpenseDataHandler}
+          />
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
     </Container>
   );
 }
---
+++ src/components/EditExpenseForm/EditExpenseForm.js
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import "../ExpenseForm/ExpenseForm.css";
+
+const EditExpenseForm = (props) => {
+  const [validated, setValidated] = useState(false);
+  const [vendorInput, setVendorInput] = useState(props.data.vendor);
+  const [dateInput, setDateInput] = useState(props.data.date);
+  const [amountInput, setAmountInput] = useState(props.data.amount);
+  const [descInput, setDescInput] = useState(props.data.desc);
+
+  const editExpenseFormSubmitHandler = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const form = e.currentTarget;
+
+    if (!form.checkValidity()) {
+      setValidated(true);
+    } else {
+      // grab the current input values
+      const editedExpenseData = {
+        vendor: vendorInput,
+        date: dateInput,
+        amount: amountInput,
+        desc: descInput,
+        id: props.data.id, // set id to old val
+      };
+
+      props.onEditExpenseSubmit(editedExpenseData);
+
+      // reset the form
+      setVendorInput("");
+      setDateInput("");
+      setAmountInput("");
+      setDescInput("");
+
+      setValidated(false);
+    }
+  };
+
+  const vendorChangeHandler = (e) => {
+    setVendorInput(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    setDateInput(e.target.value);
+  };
+
+  const amountChangeHandler = (e) => {
+    setAmountInput(e.target.value);
+  };
+
+  const descChangeHandler = (e) => {
+    setDescInput(e.target.value);
+  };
+
+  return (
+    <Form
+      noValidate
+      validated={validated}
+      onSubmit={editExpenseFormSubmitHandler}
+      className="expense-form"
+    >
+      <Row>
+        <Form.Group as={Col}>
+          <Form.Label>Vendor</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            value={vendorInput}
+            onChange={vendorChangeHandler}
+            className="input-vendor"
+          />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Date</Form.Label>
+          <Form.Control
+            required
+            type="date"
+            value={dateInput}
+            onChange={dateChangeHandler}
+            className="input-date"
+          />
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group as={Col}>
+          <Form.Label>Amount</Form.Label>
+          <Form.Control
+            required
+            type="number"
+            min={0.01}
+            step={0.01}
+            value={amountInput}
+            placeholder={"0.00"}
+            onChange={amountChangeHandler}
+            className="input-amount"
+          />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Description (Optional)</Form.Label>
+          <Form.Control
+            type="text"
+            value={descInput}
+            onChange={descChangeHandler}
+            className="input-desc"
+          />
+        </Form.Group>
+      </Row>
+      <Button className="submit-btn" variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default EditExpenseForm;
